876b5f28059918e6fdad1d2652acb127b183c182
Jean Connory <michaelrhcp@gmail.com>
2023-08-08 22:05:45 -0300

    implementando projeto web

---
+++ GeekShop/GeekShop.Web/Services/IServices/IProductService.cs
@@ -0,0 +1,17 @@
+﻿using GeekShop.Web.Models;
+
+namespace GeekShop.Web.Services.IServices
+{
+	public interface IProductService
+	{
+		Task<IEnumerable<ProductModel>> FindAllProducts();
+		
+		Task<ProductModel> FindById(long id);
+
+		Task<ProductModel> CreateProduct(ProductModel model);
+
+		Task<ProductModel> UpdateProduct(ProductModel model);
+
+		Task<bool> DeleteProductById(long id);
+	}
+}

5019da74bddae12a106e68cdc9534069cf4ec131
Jean Connory <michaelrhcp@gmail.com>
2023-08-08 22:20:24 -0300

    crud no projeto web implementado

--- GeekShop/GeekShop.Web/Program.cs
+++ GeekShop/GeekShop.Web/Program.cs
@@ -1,7 +1,13 @@
+using GeekShop.Web.Services;
+using GeekShop.Web.Services.IServices;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddHttpClient<IProductService, ProductService>(c =>
+	c.BaseAddress = new Uri(builder.Configuration["ServiceUrls:ProductAPI"]));
 
 var app = builder.Build();
 
